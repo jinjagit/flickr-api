347598cb7d31578a0de451f5b26d68c9220da496
Simon Tharby <jinjamail@gmail.com>
2019-03-21 22:52:07 +0000

    use id of photographer > 500 photos (> page max)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    person = Flickr.people.find("134813803@N03")
+    person = Flickr.people.find("24448805@N07")
     @photos = person.public_photos(per_page: 500, sizes: true).map(&:medium500!)
     @info = person.get_info!
   end
