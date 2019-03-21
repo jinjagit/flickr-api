61df4496d042fd657954bd290f5fff3b7aebf7bd
Simon Tharby <jinjamail@gmail.com>
2019-03-21 22:29:11 +0000

    basic functionality: finds first page of up to 500 photos and basic user info., given a user id

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
-    puts ENV['FLICKR_API_KEY']
     person = Flickr.people.find("134813803@N03")
-    @photos = person.public_photos(sizes: true).map(&:medium500!)
-    # Key: 4c79fdf42257f481973ffde69bfe9158
-    # Secret: 3adbe02b72c4332b
-    # my id: 134813803@N03
+    @photos = person.public_photos(per_page: 500, sizes: true).map(&:medium500!)
+    @info = person.get_info!
   end
 end
